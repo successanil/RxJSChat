306825a229a1855a582e95f29aa449365f75dabf
Anil Kukreti <anil.kukreti@paneliq.com>
2022-06-06 10:45:22 +0530

    updated rxjs initial chat

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import FirstPerson from './components/FirstPerson';
+import SecondPerson from './components/SecondPerson';
+import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />} >
+        <Route index path='/' element={<FirstPerson />} />
+        <Route index path='first-person' element={<FirstPerson />} />
+        <Route path="second-person" element={<SecondPerson />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+)
